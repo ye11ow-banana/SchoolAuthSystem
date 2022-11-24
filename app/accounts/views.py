7064f676dc8a157b6575c0e66ba7ec4742e6d410
ye11ow-banana <mikhail.yakubets@gmail.com>
2022-11-24 19:08:29 +0200

    Add permission check for login and registration

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
@@ -14,17 +15,24 @@ class HomeView(TemplateView):
     template_name = 'accounts/home.html'
 
 
-class RegistrationView(SuccessMessageMixin, CreateView):
+class RegistrationView(
+    UserPassesTestMixin, SuccessMessageMixin, CreateView
+):
     template_name = 'accounts/registration.html'
     success_url = reverse_lazy('accounts:login')
     form_class = RegistrationForm
-    success_message = "Your profile was created successfully"
+    success_message = 'Your profile was created successfully'
+    permission_denied_message = 'You have already logged in!'
+
+    def test_func(self) -> bool:
+        return not self.request.user.is_authenticated
 
 
 home_view = HomeView.as_view()
 login_view = LoginView.as_view(
     template_name='accounts/login.html',
-    next_page='accounts:home'
+    next_page='accounts:home',
+    redirect_authenticated_user=True
 )
 logout_view = LogoutView.as_view(
     template_name='accounts/logout.html',

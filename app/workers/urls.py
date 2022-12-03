030564d069fc7feb1b0983ff987957b1fdde6116
Mykhailo <mikhail.yakubets@gmail.com>
2022-12-03 11:23:54 +0200

    Add search to WLs page

--- app/workers/urls.py
+++ app/workers/urls.py
@@ -6,5 +6,6 @@ app_name = 'workers'
 
 urlpatterns = [
     path('worker_lists/', views.worker_lists_view, name='worker_lists'),
-    path('worker_list/<int:pk>/', views.worker_list_view, name='worker_list')
+    path('worker_list/<int:pk>/', views.worker_list_view, name='worker_list'),
+    path('worker_lists/search', views.worker_lists_search_view, name='worker_lists_search')
 ]
--- app/workers/views.py
+++ app/workers/views.py
@@ -1,9 +1,13 @@
+from django.db.models import QuerySet
 from django.views.generic import DetailView, ListView
 
 from workers.models import WorkerList
 
 
 class WorkerListsView(ListView):
+    """
+    View for Worker Lists page.
+    """
     model = WorkerList
     paginate_by = 4
     context_object_name = 'worker_lists'
@@ -11,10 +15,27 @@ class WorkerListsView(ListView):
 
 
 class WorkerListView(DetailView):
+    """
+    View for Worker List page.
+    """
     model = WorkerList
     context_object_name = 'worker_list'
     template_name = 'workers/worker_list.html'
 
 
+class WorkerListsSearchView(WorkerListsView):
+    """
+    Search on Worker Lists page.
+    """
+    def get_queryset(self) -> QuerySet:
+        return self.model.objects.filter(title__icontains=self.request.GET['q'])
+
+    def get_context_data(self, *args, **kwargs) -> dict:
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = f'q={self.request.GET["q"]}&'
+        return context
+
+
 worker_lists_view = WorkerListsView.as_view()
 worker_list_view = WorkerListView.as_view()
+worker_lists_search_view = WorkerListsSearchView.as_view()
